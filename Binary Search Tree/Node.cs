99089d9dd871804f39a1f74eb648a9808d3c99c4
unknown <chue.vang0@gmail.com>
2021-01-15 13:12:41 -0600

    Added member variables to the Node class, int data, Node LeftNode, and Node RightNode.  Created a constructor for the Node class.  Passing in int data into the constructor.

--- Binary Search Tree/Node.cs
+++ Binary Search Tree/Node.cs
@@ -8,6 +8,14 @@ namespace Binary_Search_Tree
 {
     class Node
     {
-           
+        public int data;
+        public Node LeftNode;
+        public Node RightNode;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+
     }
 }
